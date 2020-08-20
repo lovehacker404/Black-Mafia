918616405b06fb70fb1c273427ba61be29e94a85
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-08-20 16:47:20 +0500

    Update login.py

--- login.py
+++ login.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 # coding=utf-8
 
-#######################################################
-# File           : login.py                           #
-# Author         : DulLah                             #
-# Github         : https://github.com/dz-id           #
-# Facebook       : https://www.facebook.com/dulahz    #
-# Telegram       : https://t.me/DulLah                #
-# Python version : 3.8+                               #
-#######################################################
-#         RECODE? OKE CANTUMKAN NAMA PEMBUAT          #
-#######################################################
+######################################################
 
 import re, os, json, time
 from src import lib

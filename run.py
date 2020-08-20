a3006b93ea0fac08dd3ac5a0c0d6d5509f306f23
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-08-20 17:24:23 +0500

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 
 #######################################################
+######################BlackMafia#######################
 #######################################################
 
 import shutil, platform
@@ -9,7 +10,7 @@ import shutil, platform
 py_version = platform.python_version()
 
 if py_version < '3.7':
-    exit('WARNING anda menggunakan python version %s silahkan upgrade ke 3.7++'%(py_version))
+    exit('WARNING you are using python version %s please upgrade to 3.7++'%(py_version))
 
 cache = ['lib.py/__pycache__', 'MBF.py/__pycache__', 'store.py/__pycache__', 'app.py/__pycache__', 'CLI.py/__pycache__', 'http.py/__pycache__', 'login.py/__pycache__', 'brute.py/__pycache__', 'dump.py/__pycache__', 'fb.py/__pycache__']
 
